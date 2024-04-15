e4067d621ddda5e8d284658e4052779afd7fcb6d
Alberto <Alberto@Portatil-Alberto>
2024-04-15 15:13:32 +0200

    Creacion de la conexion con base de datos

--- Proyecto_pokemon/src/main/Main.java
+++ Proyecto_pokemon/src/main/Main.java
@@ -1,10 +1,24 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
 	public static void main(String[] args) {
-			//estopa23456
+		
+		String url = "jdbc:mysql://localhost:3306/proyecto_pokemon";
+        String usuario = "usuario";
+        String contraseña = "contraseña";
 
+        try (Connection conexion = DriverManager.getConnection(url, usuario, contraseña)) {
+            System.out.println("Conexión exitosa!");
+
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos:");
+            e.printStackTrace();
+        }
 	}
 
 }
