742624c151a764f1d609ed87d7ca1c3728d4df19
Alberto Munuera Vivo <a.munueravivo@gmail.com>
2024-04-23 10:50:07 +0200

    A

--- Proyecto_pokemon/src/controller/InicioController.java
+++ Proyecto_pokemon/src/controller/InicioController.java
@@ -1,27 +1,6 @@
 package controller;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import java.io.IOException;
 
 public class InicioController {
 
-	@FXML
-	
-	private void handleBoton(ActionEvent event) throws IOException{
-		
-		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        Scene scene = new Scene(root);
-
-
-        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
-	}
-	
-	
 }
