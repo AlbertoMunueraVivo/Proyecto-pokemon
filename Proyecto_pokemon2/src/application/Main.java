4bbd71f6e30bd8a9cafe2af8731fc986111bca8e
Alberto Munuera Vivo <a.munueravivo@gmail.com>
2024-04-25 09:51:48 +0200

    Cambio Proyecto

---
+++ Proyecto_pokemon2/src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import bbd.Conexion;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		Conexion.conexionBbd();
+		System.out.println("A");
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/inicio.fxml"));
+	        Parent root = loader.load();
+	        Scene scene = new Scene(root);
+	        
+	        primaryStage.setTitle("Proyecto Pokémon");
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+		  
+		  
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
