7d9a7b719f6ddad8da291d14a092cda08c02009d
Alberto Munuera Vivo <a.munueravivo@gmail.com>
2024-05-07 10:43:23 +0200

    Musica y centro pokemon terminado

--- Proyecto_pokemon2/src/controller/CuraController.java
+++ Proyecto_pokemon2/src/controller/CuraController.java
@@ -1,28 +1,28 @@
 package controller;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import application.EquipoPokemon;
 
 public class CuraController {
 
-    private Stage stage; // Agregar un campo para almacenar el Stage
-    
+
     @FXML
     private ImageView imagen1;
-
-    // Método para establecer el Stage desde fuera
-    public void setStage(Stage stage) {
-        this.stage = stage;
-    }//albero descargate la Cura
 
     public void initialize() {
         EquipoPokemon.Pokemon.actualizarVitalidadActualDelEquipo();
@@ -31,13 +31,14 @@ public class CuraController {
         // Crear un hilo separado para pausar sin bloquear el hilo de la UI
         new Thread(() -> {
             try {
+            	playSound(".\\sources\\hormigusica.wav");
                 Thread.sleep(10000); // Pausa de 10 segundos
                 try {
         			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CentroPokemon.fxml"));
         			Parent root = loader.load();
         			Scene scene = imagen1.getScene(); // Obtener la escena actual del botón
         			scene.setRoot(root); // Establecer la nueva raíz de la escena
-        			System.out.println("Ya estas sanado");
+        			System.out.println("Viva Pablo Motos");
         		} catch (IOException e) {
         			e.printStackTrace();
         		}
@@ -45,6 +46,22 @@ public class CuraController {
                 e.printStackTrace();
             }
         }).start();
+    }
+    private void playSound(String filePath) {
+        try {
+            File audioFile = new File(filePath);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+            clip.addLineListener(event -> {
+                if (event.getType() == LineEvent.Type.STOP) {
+                    event.getLine().close();
+                }
+            });
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
     }
 
 }
